80c700dfaf2dcc0187cce46c14ec1e0d7167d382
iamahmedshahh <artistahmed41@gmail.com>
2023-06-24 02:38:28 +0500

    empty branch

--- src/views/traderview/ToggleBot.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <v-btn depressed large color="red">Disable Asset Pair</v-btn>
-    </div>
-</template>
--- src/views/traderview/lib/wallet.js
+++
@@ -1,8 +0,0 @@
-import mm2 from './mm2MiddlewareClient'
-
-export default {
-  getWalletBalance(coin) {
-    console.log("wallet.getWalletBalance: " + coin)
-    return mm2.get(`/getBalance`)
-  }
-}
